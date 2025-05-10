5102041f82b0f77f336666a8e364aa19fb6d45b7
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-05-10 15:32:03 +0530

    Add profile edit and main profile pages, implement Navbar component with dropdown menu

---
+++ components/navbar.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useUser, useClerk } from "@clerk/nextjs";
+import Image from "next/image";
+import {
+  ChevronDown,
+  ChevronUp,
+  Command,
+  Globe,
+  LogOut,
+  Mail,
+  MessageSquare,
+  MessagesSquare,
+  Monitor,
+  MoonIcon,
+  PlusCircle,
+  RssIcon,
+  Search,
+  Sparkles,
+  SunMediumIcon,
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  const { user } = useUser();
+  const { signOut } = useClerk();
+
+  return (
+    <div className="flex items-center justify-between p-4 shadow-md">
+      <h1 className="text-lg font-semibold">Header</h1>
+
+      {user && (
+        <div className="relative">
+          {/* Dropdown Menu */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <button className="w-8 h-8 bg-transparent border-none rounded-full flex justify-center items-center">
+                <Image
+                  src={user.imageUrl}
+                  alt="User Avatar"
+                  width={35}
+                  height={35}
+                  className="rounded-full"
+                />
+              </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <Link href="/profile">
+                  <User />
+                  <span>Profile</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => alert("Billing Clicked")}>
+                Billing
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => alert("Team Clicked")}>
+                Team
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => alert("Subscription Clicked")}>
+                Subscription
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => signOut({ redirectUrl: "/" })}>
+                <LogOut />
+                <span>Sign out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
